5d7cce9866a750ed61732b7b4d84967240ecac10
raccoon2389 <roy0998@gmail.com>
2020-05-12 17:56:17 +0900

    Day01 afternoon

--- keras/day01/keras02.py
+++ keras/day01/keras02.py
@@ -17,7 +17,7 @@ model.add(Dense(3))
 model.add(Dense(1,activation='relu'))
 
 model.summary()
-'''
+
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 model.fit(x_train,y_train,epochs=100,batch_size=1,validation_data=(x_train,y_train))
 loss,acc = model.evaluate(x_test,y_test,batch_size=1)
@@ -27,4 +27,3 @@ print("acc = ",acc)
 
 output = model.predict(x_test)
 print("result : \n", output)
-'''
--- keras/day01/keras02_1.py
+++ keras/day01/keras02_1.py
@@ -10,18 +10,17 @@ x_test = np.arange(101,121)
 y_test = x_test*2
 
 model = Sequential()
-'''model.add(Dense(5000,input_dim=1,activation='relu'))
-model.add(Dense(100,activation='relu'))
+model.add(Dense(3000,input_dim=1,activation='relu'))
+model.add(Dense(30,activation='relu'))
 model.add(Dense(10,activation='relu'))
 model.add(Dense(1,activation='relu'))
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+model.summary()
 
-model.fit(x_train,y_train,epochs=1000,batch_size=5,validation_data=(x_test,y_test))
+model.fit(x_train,y_train,epochs=100,batch_size=1,validation_data=(x_test,y_test)) #batch_size defualt = 32
 loss,acc = model.evaluate(x_test,y_test,batch_size=1)
-
 print("loss : ", loss)
 print("acc = ",acc)
 
 output = model.predict(x_test)
-print("result : \n", output)'''
-model.summary()
+print("result : \n", output)

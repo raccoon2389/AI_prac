e582375782684cc9b22ed86453d403b5a20462a8
raccoon2389 <roy0998@gmail.com>
2020-05-13 09:24:00 +0900

    Day01 복습

--- keras/day01/keras02_1.py
+++ keras/day01/keras02_1.py
@@ -10,14 +10,15 @@ x_test = np.arange(101,121)
 y_test = x_test*2
 
 model = Sequential()
-model.add(Dense(3000,input_dim=1,activation='relu'))
+model.add(Dense(3000,input_dim=1,activation='relu')) #modle.add(레이어 추가)   (Dense(노드수)) relu활성화 함수
 model.add(Dense(30,activation='relu'))
 model.add(Dense(10,activation='relu'))
 model.add(Dense(1,activation='relu'))
-model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy']) #mse=mean_square_error
 model.summary()
 
-model.fit(x_train,y_train,epochs=100,batch_size=1,validation_data=(x_test,y_test)) #batch_size defualt = 32
+model.fit(x_train,y_train,epochs=100,batch_size=2,validation_data=(x_test,y_test)) #batch_size defualt = 32 epoch=반복횟수 
+                                                                                #validation_data = over-fitting을 피하기 위해 train과 test 외에 set를 준비해야하지만 여기서는 test로 대체 
 loss,acc = model.evaluate(x_test,y_test,batch_size=1)
 print("loss : ", loss)
 print("acc = ",acc)

808c27e9472777c0ba93a6d94bd59ce5daeab717
raccoon2389 <roy0998@gmail.com>
2020-05-14 17:09:01 +0900

    .

--- keras/Day03/Keras13_split.py
+++ keras/Day03/Keras13_split.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import train_test_split
 #  
 x_train, x_test, y_train, y_test = train_test_split(
     x,y,
-    random_state=99,
+    random_state=66,
     test_size=0.2, 
     shuffle=True)
 
@@ -40,7 +40,8 @@ model = Sequential()
 
 model.add(Dense(50, input_dim =1))
 model.add(Dense(20))
-model.add(Dense(30))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 #3. 훈련

44d9be0b12288909eb4431a5bdbf8f6c24a2d8ef
raccoon2389 <roy0998@gmail.com>
2020-05-14 17:29:02 +0900

    .

--- keras/Day03/Keras13_split.py
+++ keras/Day03/Keras13_split.py
@@ -38,10 +38,9 @@ from keras.models import Sequential
 from keras.layers import Dense
 model = Sequential()
 
-model.add(Dense(50, input_dim =1))
-model.add(Dense(20))
-model.add(Dense(50))
+model.add(Dense(10, input_dim =1))
 model.add(Dense(10))
+model.add(Dense(5))
 model.add(Dense(1))
 
 #3. 훈련
@@ -52,7 +51,7 @@ model.compile(loss='mse', optimizer='adam', metrics=['mse'])    # mse 평균제�
                                                                 # 회귀 = 1.54 , 10.01 같이 연속적인 값 
                                                                 # metrics는 loss처럼 훈련에 영향은 주지 않고 계산한 값만 뱉어냄
 
-model.fit(x_train,y_train, epochs=110, batch_size=1, validation_split=0.25) # epoch = 훈련 횟수 ; 일정수 이상의 훈련을 반복하면 과적합(over-fitting)이 일어나게 된다. 
+model.fit(x_train,y_train, epochs=125, batch_size=1, validation_split=0.25) # epoch = 훈련 횟수 ; 일정수 이상의 훈련을 반복하면 과적합(over-fitting)이 일어나게 된다. 
                                                                                     # validation set = train set 중 일부를 떼와서 train으로 훈련후 검증한다
                                                                                     # fit하는 과정에 반영이 된다. W 값 최적화에 도움이 됨
                                                                                     # test는 최종 확인만 하므로 fit 과정에 영향을 주지 않음

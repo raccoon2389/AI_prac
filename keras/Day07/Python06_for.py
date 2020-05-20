c1c7f1d755f92ca52f0ed2e5b2fb7d9215008b1e
raccoon2389 <roy0998@gmail.com>
2020-05-20 10:17:46 +0900

    .

--- keras/Day07/Python06_for.py
+++ keras/Day07/Python06_for.py
@@ -2,3 +2,13 @@
 a = {'name': 'Q', 'phone' : '010', 'birth' : '0809'}
 for i in a.values():
     print(i)
+
+a = range(10)
+for i in a:
+    i = i*i
+    print(i)
+# print('메렁')
+
+#while 조건문 : #참일동안 계속된다.
+
+while 

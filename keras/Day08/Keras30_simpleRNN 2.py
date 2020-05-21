8b0070b8eba4552da177cc518cb322f17fa828ad
raccoon2389 <roy0998@gmail.com>
2020-05-21 15:19:17 +0900

    .

--- keras/Day08/Keras30_simpleRNN 2.py
+++ keras/Day08/Keras30_simpleRNN 2.py
@@ -20,8 +20,9 @@ from keras.layers import Dense,SimpleRNN
 from keras.callbacks import EarlyStopping
 
 #1. 데이터
-x = np.array([[1,2,3],[2,3,4],[3,4,5],[4,5,6]])
-y = np.array([4,5,6,7])
+x = np.array([[1,2,3],[2,3,4],[3,4,5],[4,5,6],[5,6,7],[6,7,8],[7,8,9],[8,9,10],[9,10,11],[10,11,12],[20,30,40],[30,40,50],[40,50,60]])
+y = np.array([4,5,6,7,8,9,10,11,12,13,50,60,70])
+
 
 print("shape of x : ", x.shape)
 
@@ -42,10 +43,11 @@ model = Sequential()
 # model.add(LSTM(10,input_shape=(3,1)))
 model.add(SimpleRNN(10,input_length=3, input_dim=1))
 
+model.add(Dense(50))
+model.add(Dense(40))
 model.add(Dense(30))
 model.add(Dense(10))
 model.add(Dense(5))
-model.add(Dense(3))
 model.add(Dense(1))
 
 model.summary() # LSTM 파라미터 폭증 이유 알아오기
@@ -53,14 +55,14 @@ model.summary() # LSTM 파라미터 폭증 이유 알아오기
 # 실행
 model.compile(optimizer='adam', loss='mse')
 
-e_stop = EarlyStopping(monitor='loss',mode='auto',patience=50)
+e_stop = EarlyStopping(monitor='loss',mode='auto',patience=100)
 model.fit(x,y,epochs=10000,callbacks=[e_stop])
 
-x_predict = np.array([5,6,7])
-x_predict = x_predict.reshape(1,3,1)
+x_input = np.array([50,60,70])
+x_input = x_input.reshape(1,3,1)
+y_test = np.array([[80]])
+print(x_input)
 
-print(x_predict)
 
-y_predict = model.predict(x_predict)
-
-print(y_predict)
+yhat = model.predict(x_input)
+print(yhat)
